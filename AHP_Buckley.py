f00f6456882c3e919ad34e0e966897b4d23c0e47
Emma <eredfoot@gmail.com>
2018-03-12 09:50:44 -0600

    Writing out Performance Scores and Fuzzy Weights

--- AHP_Buckley.py
+++ AHP_Buckley.py
@@ -1,6 +1,23 @@
 import numpy
 import sympy
 
+#Functions
+def GeometricMean(LineList, Spot):
+    values = [x[Spot] for x in LineList]
+    GeoMean=1
+    for i in range(len(LineList)):
+        GeoMean=GeoMean*values[i]
+    return(GeoMean**(1/len(LineList)))
+
+def PerformanceScores(a, b, c, d):
+    # The number of rows in each matrix will always be three, so I am hard coding it in
+    PS=[0]*3
+    for x in range(3):
+        PS[x]=[a[x]/sum(d), b[x]/sum(c), c[x]/sum(b), d[x]/sum(a)]
+    return(PS)
+
+
+#CONSTANTS AND INPUT
 #1 corresponds to Equally important
 #2-3 correspond to weakly more important
 #4-5 corresponds to strongly more important
@@ -23,7 +40,7 @@ SafetyLine1=[Equal, Equal, Weak,Strong,Strong, Strong]
 SafetyLine2=[InvWeak, InvStrong, Equal, Equal, EqI,EqI]
 SafetyLine3=[InvStrong, InvStrong,InvEqI, InvEqI,Equal, Equal]
 
-FluctuateLine1 = [Equal, Equal,EqI,EqI,Strong, Abs],
+FluctuateLine1 = [Equal, Equal,EqI,EqI,Strong, Abs]
 FluctuateLine2= [InvEqI, InvEqI, Equal, Equal, Abs, Abs]
 FluctuateLine3= [InvStrong, InvAbs, InvAbs, InvAbs, Equal, Equal]
 
@@ -35,59 +52,24 @@ CharLine1=[Equal, Equal,Very,Abs,Abs, Abs]
 CharLine2=[InvVery, InvAbs, Equal, Equal, Abs, Very]
 CharLine3=[InvAbs, InvAbs, InvAbs, InvVery, Equal, Equal]
 
-def GeometricMean(LineList, Spot):
-    values = [x[Spot] for x in LineList]
-    GeoMean=1
-    for i in range(len(LineList)):
-        GeoMean=GeoMean*values[i]
-    return(GeoMean**(1/len(LineList)))
+#Find the Geometric Mean for Each of the matrices and each location
+SafeSpot0 = [GeometricMean(SafetyLine1, 0), GeometricMean(SafetyLine2, 0), GeometricMean(SafetyLine3, 0)]
+SafeSpot1 = [GeometricMean(SafetyLine1, 1), GeometricMean(SafetyLine2, 1),GeometricMean(SafetyLine3, 1)]
+SafeSpot2 = [GeometricMean(SafetyLine1, 2), GeometricMean(SafetyLine2, 2),GeometricMean(SafetyLine3, 2)]
+SafeSpot3 = [GeometricMean(SafetyLine1, 3), GeometricMean(SafetyLine2, 3),GeometricMean(SafetyLine3, 3)]
 
-a= [GeometricMean(SafetyLine1, 0), GeometricMean(SafetyLine2, 0), GeometricMean(SafetyLine3, 0)]
-b= [GeometricMean(SafetyLine1, 1), GeometricMean(SafetyLine2, 1),GeometricMean(SafetyLine3, 1)]
-c = [GeometricMean(SafetyLine1, 2), GeometricMean(SafetyLine2, 2),GeometricMean(SafetyLine3, 2)]
-d = [GeometricMean(SafetyLine1, 3), GeometricMean(SafetyLine2, 3),GeometricMean(SafetyLine3, 3)]
+FlucSpot0 =  [GeometricMean(FluctuateLine1, 0), GeometricMean(FluctuateLine2, 0), GeometricMean(FluctuateLine3, 0)]
+FlucSpot1  = [GeometricMean(FluctuateLine1, 1), GeometricMean(FluctuateLine2, 1), GeometricMean(FluctuateLine3, 1)]
+FlucSpot2  = [GeometricMean(FluctuateLine1, 2), GeometricMean(FluctuateLine2, 2), GeometricMean(FluctuateLine3, 2)]
+FlucSpot3  = [GeometricMean(FluctuateLine1, 3), GeometricMean(FluctuateLine2, 3), GeometricMean(FluctuateLine3, 3)]
 
-PerformanceScore_11 = [a[0]/sum(d), b[0]/sum(c), c[0]/sum(b), d[0]/sum(a)]
-PerformanceScore_12 = [a[1]/sum(d), b[1]/sum(c), c[1]/sum(b), d[1]/sum(a)]
-PerformanceScore_13 = [a[1]/sum(d), b[1]/sum(c), c[1]/sum(b), d[1]/sum(a)]
-
-def PerformanceScores(a, b, c, d):
-    # The number of rows in each matrix will always be three, so I am hard coding it in
-    PS=[0]*3
-    for x in range(3):
-        PS[x]=[a[x]/sum(d), b[x]/sum(c), c[x]/sum(b), d[x]/sum(a)]
-    return(PS)
-
-print(PerformanceScores(a, b, c, d))
-
-# def PerformanceScores(LineList, MeanSum):
-#     R=[0]*len(LineList)
-#     for i in range(len(Equal)):
-#         print("i= ", i)
-#         R[i]=GeometricMean(LineList, i)
-#         print(R[i])
-#
-#     # r=GeometricMean(LineList, Spot)/MeanSum
-#     # return(r)
-#
-# R11=PerformanceScores(SafetyLine1, Safety4Mean)
+CharSpot0 =  [GeometricMean(CharLine1, 0), GeometricMean(CharLine2, 0), GeometricMean(CharLine3, 0)]
+CharSpot1  = [GeometricMean(CharLine1, 1), GeometricMean(CharLine2, 1), GeometricMean(CharLine3, 1)]
+CharSpot2  = [GeometricMean(CharLine1, 2), GeometricMean(CharLine2, 2), GeometricMean(CharLine3, 2)]
+CharSpot3  = [GeometricMean(CharLine1, 3), GeometricMean(CharLine2, 3), GeometricMean(CharLine3, 3)]
 
 
-# Safety1Mean = GeometricMean(SafetyLine1, 0) + GeometricMean(SafetyLine2, 0)+GeometricMean(SafetyLine3, 0)
-# Safety2Mean = GeometricMean(SafetyLine1, 1) + GeometricMean(SafetyLine2, 1)+GeometricMean(SafetyLine3, 1)
-# Safety3Mean = GeometricMean(SafetyLine1, 2) + GeometricMean(SafetyLine2, 2)+GeometricMean(SafetyLine3, 2)
-# Safety4Mean = GeometricMean(SafetyLine1, 3) + GeometricMean(SafetyLine2, 3)+GeometricMean(SafetyLine3, 3)
-
-# Safety1Mean = GeometricMean(SafetyLine1, 0) + GeometricMean(SafetyLine2, 0)+GeometricMean(SafetyLine3, 0)
-# print(Safety1Mean)
-# Safety2Mean = GeometricMean(SafetyLine1, 1) + GeometricMean(SafetyLine2, 1)+GeometricMean(SafetyLine3, 1)
-# Safety3Mean = GeometricMean(SafetyLine1, 2) + GeometricMean(SafetyLine2, 2)+GeometricMean(SafetyLine3, 2)
-# Safety4Mean = GeometricMean(SafetyLine1, 3) + GeometricMean(SafetyLine2, 3)+GeometricMean(SafetyLine3, 3)
-
-
-#SafetyPS_11 = (GeometricMean(SafetyLine1,0)/Safety1Mean)
-
-# Fluctuate1Mean = GeometricMean(FluctuateLine1, 0) + GeometricMean(FluctuateLine2, 0)+GeometricMean(FluctuateLine3, 0)
-# Fluctuate2Mean = GeometricMean(FluctuateLine1, 1) + GeometricMean(FluctuateLine2, 1)+GeometricMean(FluctuateLine3, 1)
-# Fluctuate3Mean = GeometricMean(FluctuateLine1, 2) + GeometricMean(FluctuateLine2, 2)+GeometricMean(FluctuateLine3, 2)
-# Fluctuate4Mean = GeometricMean(FluctuateLine1, 3) + GeometricMean(FluctuateLine2, 3)+GeometricMean(FluctuateLine3, 3)
+print("The Performance Scores indicate the relative strength of each pair of elements in the same hierarchy \n")
+print("Safety Performance Scores: ", PerformanceScores(SafeSpot0, SafeSpot1, SafeSpot2, SafeSpot3))
+print("\nFluctuation Performance Scores: ", PerformanceScores(FlucSpot0, FlucSpot1, FlucSpot2, FlucSpot3))
+print("\n The Fuzzy Weights are: ", PerformanceScores(CharSpot0, CharSpot1, CharSpot2, CharSpot3))
